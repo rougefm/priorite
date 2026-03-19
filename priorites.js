d2ea068db0f9248f5bc6e2f07f7a86cb16d33e9d
rougefm <jean-philippe.marcil@bellmedia.ca>
2026-03-19 19:33:03 -0400

    Create priorites.js

---
+++ priorites.js
@@ -0,0 +1,5 @@
+const mesPriorites = [
+    "Mentionner le concours Véro et Rouge à 7h10.",
+    "Rappeler que les billets de Dua Lipa sont en vente ce matin.",
+    "Féliciter la gagnante d'hier, Sylvie de Brossard."
+];
