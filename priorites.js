f90da4e9b0e9056aaa1692529c233e5a11159012
rougefm <jean-philippe.marcil@bellmedia.ca>
2026-03-19 19:59:38 -0400

    Update priorites.js

--- priorites.js
+++ priorites.js
@@ -1,5 +1,30 @@
-const mesPriorites = [
-    "Mentionner le concours Véro et Rouge à 7h10.",
-    "Rappeler que les billets de Dua Lipa sont en vente ce matin.",
-    "Féliciter la gagnante d'hier, Sylvie de Brossard."
-];
+const prioritesSemaine = {
+    "lundi": [
+        "Dès 9h: 2h sans interruption + Mega Mix Payants - Jusqu'à 10 000$ à gagner",
+        "Gagnez votre voyage au Club Med grâce à Pipi et Cricri",
+        "15h55: Ne manquez pas le retour à la maison musicale francophone #1 au pays"
+    ],
+    "mardi": [
+        "Mettez de l'avant l'écoute en ligne - Minimum 1x par show",
+        "Le concours de la semaine continue...",
+        "Parler de la mise en vente de Lady Gaga"
+    ],
+    "mercredi": [
+        "Grosse annonce à 16h00 !",
+        "Tirage : Un voyage à Toronto pour voir Bruno Mars"
+    ],
+    "jeudi": [
+        "C'est le Jeudi Rétro !",
+        "N'oubliez pas de plugger le show des Trois Accords"
+    ],
+    "vendredi": [
+        "La compil weekend avec Denis et Gégé: Est-ce que Gone Gone Gone sera toujours #1",
+        "VHDW: On débute avec 2h et vos billets pour Gaga"
+    ],
+    "samedi": [
+        "C'est le weekend ! Rappel du concours web."
+    ],
+    "dimanche": [
+        "Bon dimanche ! Mettre de l'avant l'application iHeartRadio."
+    ]
+};
