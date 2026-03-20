edd4ce4fcb1d6ccca98fe8d66fb1e218a99374c3
rougefm <jean-philippe.marcil@bellmedia.ca>
2026-03-20 17:04:35 -0400

    Update journees.js

--- journees.js
+++ journees.js
@@ -1,6 +1,6 @@
 /**
  * journees.js
- * Base de données complète extraite du calendrier "Journees.xlsx".
+ * Base de données complète pour l'année 2026.
  * Format de clé : "JJ-MM" pour permettre un scan chronologique automatique.
  */
 const journeesMondiales = {
@@ -20,7 +20,7 @@ const journeesMondiales = {
 
     // FÉVRIER
     "01-02": ["Semaine des enseignants (Début)"],
-    "02-02": ["Journée de la marmotte / Journée mondiale des zones humides"],
+    "02-02": ["Journée de la marmotte", "Journée mondiale des zones humides"],
     "04-02": ["Journée mondiale contre le cancer"],
     "05-02": ["Journée mondiale du Nutella"],
     "13-02": ["Journée mondiale de la radio"],
@@ -34,6 +34,35 @@ const journeesMondiales = {
     "20-03": ["Journée internationale de la Francophonie", "Journée mondiale du bonheur"],
     "21-03": ["Journée mondiale de la poésie"],
 
+    // AVRIL
+    "01-04": ["Poisson d'avril (Journée du rire)"],
+    "02-04": ["Journée mondiale de sensibilisation à l'autisme"],
+    "07-04": ["Journée mondiale de la santé"],
+    "15-04": ["Journée mondiale de l'art"],
+    "22-04": ["Journée mondiale de la Terre"],
+    "23-04": ["Journée mondiale du livre et du droit d'auteur"],
+
+    // MAI
+    "01-05": ["Fête du Travail"],
+    "03-05": ["Journée mondiale de la liberté de la presse"],
+    "12-05": ["Journée internationale des infirmières"],
+    "15-05": ["Journée internationale des familles"],
+    "17-05": ["Journée mondiale contre l'homophobie et la transphobie"],
+    "31-05": ["Journée mondiale sans tabac"],
+
+    // JUIN
+    "05-06": ["Journée mondiale de l'environnement"],
+    "08-06": ["Journée mondiale de l'océan"],
+    "14-06": ["Journée mondiale du donneur de sang"],
+    "21-06": ["Journée nationale des peuples autochtones", "Fête de la musique"],
+    "24-06": ["Fête nationale du Québec (Saint-Jean-Baptiste)"],
+
+    // JUILLET
+    "01-07": ["Fête du Canada", "Journée nationale du déménagement (Québec)"],
+    "11-07": ["Journée mondiale de la population"],
+    "17-07": ["Journée mondiale des émojis"],
+    "30-07": ["Journée internationale de l'amitié"],
+
     // AOÛT
     "07-08": ["Journée mondiale de l’amitié", "Journée nationale des sœurs"],
     "15-08": ["Journée nationale de la relaxation"],
@@ -45,6 +74,7 @@ const journeesMondiales = {
     "05-09": ["Journée pour être en retard à quelque chose"],
     "07-09": ["Fête du Travail (Férié)"],
     "20-09": ["Semaine internationale du bonheur au travail"],
+    "21-09": ["Journée internationale de la paix"],
     "28-09": ["Journée du bon voisinage"],
     "30-09": ["Journée de la vérité et de la réconciliation", "Journée internationale du podcast"],
 
@@ -58,11 +88,11 @@ const journeesMondiales = {
 
     // NOVEMBRE
     "03-11": ["Journée de sensibilisation au stress"],
-    "19-11": ["Journée internationale des hommes / Toilettes"],
+    "19-11": ["Journée internationale des hommes", "Journée mondiale des toilettes"],
     "21-11": ["Journée mondiale de la télévision"],
     "23-11": ["Journée de l’espresso"],
     "25-11": ["Journée mondiale du jeu vidéo"],
-    "26-11": ["Journée sans achats / Gâteau / Maïs / Soie dentaire"],
+    "26-11": ["Journée sans achats", "Journée du gâteau", "Journée du maïs"],
     "27-11": ["Black Friday (Date mobile)"],
     "28-11": ["Journée du pain doré"],
     "30-11": ["Cyber Monday (Date mobile)"],
@@ -70,10 +100,10 @@ const journeesMondiales = {
     // DÉCEMBRE
     "03-12": ["Journée des cadres scolaires (AQCS)"],
     "04-12": ["Jour du biscuit"],
-    "05-12": ["Journée mondiale du bénévolat / Volontaires"],
-    "07-12": ["Journée de la barbe à papa / Brownie"],
-    "09-12": ["Journée de la pâtisserie / Carte de Noël"],
-    "10-12": ["Journée des droits de l’homme / Bière blonde"],
+    "05-12": ["Journée mondiale du bénévolat"],
+    "07-12": ["Journée de la barbe à papa", "Journée du brownie"],
+    "09-12": ["Journée de la pâtisserie", "Journée de la carte de Noël"],
+    "10-12": ["Journée des droits de l’homme", "Journée de la bière blonde"],
     "11-12": ["Journée Mange un bagel"],
     "24-12": ["Veille de Noël"],
     "25-12": ["Noël (Férié)"],
