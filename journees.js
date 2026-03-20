8698f19a31f0a66fedcd6007d2cd8bbca8c8f00f
rougefm <jean-philippe.marcil@bellmedia.ca>
2026-03-20 13:46:45 -0400

    Create journees.js

---
+++ journees.js
@@ -0,0 +1,35 @@
+/**
+ * journees.js
+ * Liste des journées internationales et mondiales par jour de la semaine.
+ * Idéal pour trouver des sujets de discussion "Talk" en ondes.
+ */
+const journeesMondiales = {
+    "lundi": [
+        "Journée mondiale de la poésie",
+        "Journée internationale pour l'élimination de la discrimination raciale"
+    ],
+    "mardi": [
+        "Journée mondiale de l'eau",
+        "Journée internationale de la Francophonie"
+    ],
+    "mercredi": [
+        "Journée mondiale de la météo",
+        "Journée internationale du bonheur"
+    ],
+    "jeudi": [
+        "Journée mondiale de la lutte contre la tuberculose",
+        "Journée internationale du droit à la vérité"
+    ],
+    "vendredi": [
+        "Journée mondiale de la vie sauvage",
+        "Journée internationale de la visibilité transgenre"
+    ],
+    "samedi": [
+        "Journée mondiale du théâtre",
+        "Une heure pour la planète (Earth Hour)"
+    ],
+    "dimanche": [
+        "Journée mondiale de la sensibilisation à l'autisme",
+        "Journée internationale du rire"
+    ]
+};
